a226a60c1bc20db1518ad6eff87789b166c82574
Vinícius Rodrigues da Rosa <vinicius.rosa2004@gmail.com>
2024-11-19 11:39:49 -0300

    ajustes controller (ainda sem conseguir acessar o banco)

--- Treino_aplicacao_web/Controllers/EmpresaController.cs
+++ Treino_aplicacao_web/Controllers/EmpresaController.cs
@@ -4,6 +4,7 @@ using System.Web.Mvc;
 using Treino_aplicacao_web.Database;
 using System.Data.Entity;
 using Treino_aplicacao_web.ViewModel.Empresa;
+using Treino_aplicacao_web.Models.Empresa;
 
 namespace Treino_aplicacao_web.Controllers
 {
@@ -33,6 +34,129 @@ namespace Treino_aplicacao_web.Controllers
                 })
                 .ToListAsync()
             );
+        }
+
+        public async Task<ActionResult> Detalhes(int id) 
+        {
+            var empresa = await _context.Empresa.FirstOrDefaultAsync(i => i.Id == id);
+            if (empresa == null) return HttpNotFound();
+
+            var viewModel = new EmpresaDetalhesViewModel
+            {
+                Id = empresa.Id,
+                Nome = empresa.Nome,
+                CNPJ = empresa.CNPJ,
+                Pais = empresa.Pais,
+                Endereco = empresa.Endereco,
+                NumeroDoEndereco = empresa.NumeroDoEndereco,
+                CEP = empresa.CEP
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Criar()
+        {
+            return View(new EmpresaFormViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Criar(EmpresaFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid) return View(viewModel);
+
+            var empresa = new Empresa
+            {
+                Id = viewModel.Id,
+                Nome = viewModel.Nome,
+                CNPJ = viewModel.CNPJ,
+                Pais = viewModel.Pais,
+                Endereco = viewModel.Endereco,
+                NumeroDoEndereco= viewModel.NumeroDoEndereco,
+                CEP = viewModel.CEP
+            };
+
+            _context.Empresa.Add(empresa);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> Editar(int id)
+        {
+            var empresa = await _context.Empresa.FirstOrDefaultAsync(i => i.Id == id);
+            if (empresa == null) return HttpNotFound();
+
+            var viewModel = new EmpresaFormViewModel
+            {
+                Id = empresa.Id,
+                Nome = empresa.Nome,
+                CNPJ = empresa.CNPJ,
+                Pais = empresa.Pais,
+                Endereco = empresa.Endereco,
+                NumeroDoEndereco = empresa.NumeroDoEndereco,
+                CEP = empresa.CEP
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Editar(EmpresaFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid) return View(viewModel);
+
+            var empresa = await _context.Empresa.SingleOrDefaultAsync(i => i.Id == viewModel.Id);
+            if (empresa == null) return HttpNotFound();
+
+            empresa.Nome = viewModel.Nome;
+            empresa.CNPJ = viewModel.CNPJ;
+            empresa.Pais = viewModel.Pais;
+            empresa.Endereco = viewModel.Endereco;
+            empresa.NumeroDoEndereco= viewModel.NumeroDoEndereco;
+            empresa.CEP = viewModel.CEP;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> Deletar(int id)
+        {
+            var empresa = await _context.Empresa.FirstOrDefaultAsync(i => i.Id == id);
+            if (empresa == null) return HttpNotFound();
+
+            var viewModel = new EmpresaDeletaViewModel
+            {
+                Id = id,
+                Nome = empresa.Nome,
+                CNPJ = empresa.CNPJ
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeletarConfirmado(int id)
+        {
+            var empresa = await _context.Empresa.FirstOrDefaultAsync(i => i.Id == id);
+            if (empresa == null) return HttpNotFound();
+
+            _context.Empresa.Remove(empresa);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing) 
+        {
+            if (disposing) 
+            {
+                _context.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
